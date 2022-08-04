3f461be2b55164d914d25a55536f3a8199c9cf3a
ajhein <ajhein@iu.edu>
2022-08-03 19:15:56 -0700

    added playlist section

--- components/sidebar.js
+++ components/sidebar.js
@@ -38,6 +38,8 @@ function Sidebar() {
                     <p>Your Episodes</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"/>
+
+                <p className="cursor-pointer hover:text-white">playlist name</p>
             </div>
         </div>
     );
