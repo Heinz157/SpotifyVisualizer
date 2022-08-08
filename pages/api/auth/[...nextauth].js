0cec409354e97ac0813274bccfc5b54b6eb93288
ajhein <ajhein@iu.edu>
2022-08-07 18:17:23 -0700

    building token handling process

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { callbackify } from "util";
 import { LOGIN_URL } from "../../../lib/spotify";
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +12,32 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/Login',
+  },
+  callbacks: {
+  async jwt({token, account, user}) {
+
+    // initial sign in
+    if (account && user) {
+        return {
+            ...token,
+            accessToken: account.access_token,
+            refreshToken: account.refresh_token,
+            username: account.providerAccountId,
+            accessTokenExpires: account.expires_at * 1000, // convert to ms
+        }
+    }
+
+    // refresh token
+    if (Date.now() < token.accessTokenExpires) {
+        console.log('Exisiting token is valid')
+        return token;
+    }
+    // New Access Token
+    console.log('Exisiting token is expired')
+    return await refreshAccessToken(token);
+    }
+  } 
 })
