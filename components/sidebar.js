06d9cfcf23dd402446d882f2bf6998c6dfec9b19
ajhein <ajhein@iu.edu>
2022-08-07 19:08:55 -0700

    syntax fix

--- components/sidebar.js
+++ components/sidebar.js
@@ -13,33 +13,33 @@ function Sidebar() {
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
             <div className="space-y-4">
             <button className="flex items-center space-x-2 hover:text-white">
-                    <HomeIcon clasName="h-5 w-5" />
+                    <HomeIcon className="h-5 w-5" />
                     <p>Logout</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
-                    <HomeIcon clasName="h-5 w-5" />
+                    <HomeIcon className="h-5 w-5" />
                     <p>Home</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <SearchIcon clasName="h-5 w-5" />
+                    <SearchIcon className="h-5 w-5" />
                     <p>Search</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <LibraryIcon clasName="h-5 w-5" />
+                    <LibraryIcon className="h-5 w-5" />
                     <p>Your Library</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"/>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <PlusCircleIcon clasName="h-5 w-5" />
+                    <PlusCircleIcon className="h-5 w-5" />
                     <p>Create Playlist</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <HeartIcon clasName="h-5 w-5" />
+                    <HeartIcon className="h-5 w-5" />
                     <p>Liked Songs</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <RssIcon clasName="h-5 w-5" />
+                    <RssIcon className="h-5 w-5" />
                     <p>Your Episodes</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"/>
