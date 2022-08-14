d7e900acb00487f95fb70926c2186eb145522dff
ajhein <ajhein@iu.edu>
2022-08-13 18:59:16 -0700

    Song Render + signout moved

--- components/sidebar.js
+++ components/sidebar.js
@@ -27,13 +27,10 @@ function Sidebar() {
             });
         }
     }, [session, spotifyApi]);
-    
+
     return (
         <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w[12rem] lg:max[15rem] hidden md:inline-flex">
             <div className="space-y-4">
-            <button className="flex items-center space-x-2 hover:text-white">
-                    <p>Logout</p>
-                </button>
                 <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
                     <HomeIcon className="h-5 w-5" />
                     <p>Home</p>
@@ -46,7 +43,7 @@ function Sidebar() {
                     <LibraryIcon className="h-5 w-5" />
                     <p>Your Library</p>
                 </button>
-                <hr className="border-t-[0.1px] border-gray-900"/>
+                <hr className="border-t-[0.1px] border-gray-900" />
 
                 <button className="flex items-center space-x-2 hover:text-white">
                     <PlusCircleIcon className="h-5 w-5" />
@@ -60,12 +57,12 @@ function Sidebar() {
                     <RssIcon className="h-5 w-5" />
                     <p>Your Episodes</p>
                 </button>
-                <hr className="border-t-[0.1px] border-gray-900"/>
+                <hr className="border-t-[0.1px] border-gray-900" />
 
                 {playlists.map((playlist) => (
-                <p key={playlist.id}
-                onClick={() => setPlaylistid(playlist.id)}
-                className="cursor-pointer hover:text-white">{playlist.name}</p>
+                    <p key={playlist.id}
+                        onClick={() => setPlaylistid(playlist.id)}
+                        className="cursor-pointer hover:text-white">{playlist.name}</p>
                 ))}
             </div>
         </div>
