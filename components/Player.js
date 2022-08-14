cb2ac74b8e2a9a35730780b059a62e85a46ed3bf
ajhein <ajhein@iu.edu>
2022-08-13 19:54:19 -0700

    Player Changes

--- components/Player.js
+++ components/Player.js
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 import {useState} from 'react';
 import {useRecoilState} from 'recoil';
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
 
 function Player() {
     const spotifyApi = useSpotify();
@@ -11,10 +12,11 @@ function Player() {
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     const [volume, setVolume] = useState(50);
 
+    const songInfo = useSongInfo();
     return (
         <div>
             <div>
-                <img src="" alt=""/>
+                <img src={songInfo?.album.images?.[0].url} alt=""/>
             </div>
         </div>
     );
